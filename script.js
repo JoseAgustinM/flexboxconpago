8582381739b6dcab1c52d3ee0ca4ba73bfa7cfec
JoseAgustinM <115422933+JoseAgustinM@users.noreply.github.com>
2023-07-03 22:47:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+function name(arr){
+console.log(arr[0]);
+};
+name([18,15]);
+
+function saludar(){
+    const age=18;
+     document.write(age);
+    };
+    function isGreater(a, b) { 
+        if (a > b > c) { 
+         console.log("el valor mayor es: " + a);}
+         else if (b > a > c) { 
+         console.log("el valor mayor es: " + b); 
+        }  
+        else { 
+            console.log("el valor mayor es: " + c);}
+        }  
+       isGreater(13, 10, 30);
+       function getAlert(){
+        alert("are you minor?");
+        };
+        function isMinor(){
+            let edad;
+            do{
+               edad = confirm("are you minor?");
+            }
+            while(edad);
+        }
+        
+           
